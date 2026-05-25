3267976f6ebd1f9b88dbf149e3bdc687e8e27814
GustaPandini <gugui.pandini@gmail.com>
2026-05-25 19:34:29 -0300

    feat: Adicionada as validações básicas ultilizando a Bibiloteca Data Annotations na entity Automovel

--- Entity/Automovel.cs
+++ Entity/Automovel.cs
@@ -1,18 +1,39 @@
-﻿namespace ConsoleApp2.Entity
+﻿namespace ConsoleApp2.Entity;
+
+using System.ComponentModel.DataAnnotations;
+
+public class Automovel
 {
-    public class Automovel
-    {
-        public int Id { get; set; }
-        public string Marca { get; set; }
-        public string Modelo { get; set; }
-        public string Powertrain { get; set; }
-        public string Versao { get; set; }
-        public string Cor { get; set; }
-        public int Ano { get; set; }
-        public int AnoModelo { get; set; }
-        public int Quilometragem { get; set; }
-        public decimal Preco { get; set; }
-        public bool Blindado { get; set; }
-        public int QuantidadeDonos { get; set; }
-    }
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "A Marca é obrigatória.")]
+    public string Marca { get; set; }
+
+    [Required(ErrorMessage = "O Modelo é obrigatório.")]
+    public string Modelo { get; set; }
+
+    [Required(ErrorMessage = "O PowerTrain é obrigatório.")]
+    public string Powertrain { get; set; }
+
+    [Required(ErrorMessage = "A Versão é obrigatória.")]
+    public string Versao { get; set; }
+
+    [Required(ErrorMessage = "A Cor é obrigatória.")]
+    public string Cor { get; set; }
+
+    [Range(1886, int.MaxValue, ErrorMessage = "O ano deve ser maior que 1886.")]
+    public int Ano { get; set; }
+
+    public int AnoModelo { get; set; }
+
+    [Range(0, int.MaxValue, ErrorMessage = "A quilometragem não pode ser negativa.")]
+    public int Quilometragem { get; set; }
+
+    [Range(0.01, double.MaxValue, ErrorMessage = "O preço deve ser maior que 0.")]
+    public decimal Preco { get; set; }
+
+    public bool Blindado { get; set; }
+
+    [Range(0, int.MaxValue, ErrorMessage = "A quantidade de donos não pode ser negativa.")]
+    public int QuantidadeDonos { get; set; }
 }
