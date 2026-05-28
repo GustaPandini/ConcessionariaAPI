bb08d8d51764e1044607c5f60017909864947ce3
GustaPandini <gugui.pandini@gmail.com>
2026-05-27 21:21:59 -0300

    refactor: Remoção de funções não mais ultilizadas dentro da classe Service

--- Service/AutomovelService.cs
+++ Service/AutomovelService.cs
@@ -32,112 +32,28 @@ namespace ConsoleApp2.Services
             }
         }
 
-        public void Alterar(Automovel automovel) => _repository.Alterar(automovel);
+        public void Alterar(Automovel automovel)
+        {
+            if (VerificarAnoValido(automovel))
+            {
+                if (VerificarAnoModeloValido(automovel))
+                {
+                    _repository.Alterar(automovel);
+                }
+                else
+                {
+                    throw new ArgumentException("Ano do modelo inválido. O ano do modelo deve ser igual ou um ano superior ao ano do automóvel.");
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Ano inválido. O ano deve ser entre 1886 e o ano atual.");
+            }
+        }
+
         public void Deletar(Automovel automovel) => _repository.Deletar(automovel);
         public List<Automovel> Listar() => _repository.Listar();
         public Automovel ObterPorId(int id) => _repository.MostrarAutomovelPorId(id);
-
-        public bool ValidacaoValorString(string input)
-        {
-            if (string.IsNullOrWhiteSpace(input))
-            {
-                return false;
-            }
-            return true;
-        }
-
-        public bool LerValidacaoBlindado(string input, out bool blindado)
-        {
-            blindado = false;
-            if (input == "sim")
-            {
-                blindado = true;
-                return true;
-            }
-            else if (input == "não" || input == "nao")
-            {
-                blindado = false;
-                return true;
-            }
-            return blindado;
-        }
-
-        public string MostrarBlindagem(Automovel automovel)
-        {
-            string blindado;
-            if (automovel.Blindado == true)
-            {
-                blindado = "Sim";
-                return blindado;
-            }
-            else
-            {
-                blindado = "Não";
-                return blindado;
-            }
-        }
-        public string VerificarAlteracaoString(string input, string valorAntigo)
-        {
-            if (string.IsNullOrWhiteSpace(input))
-            {
-                return valorAntigo;
-            }
-            else
-            {
-                return input;
-            }
-        }
-        public bool VerificarAlteracaoInt(string input)
-        {
-            if (string.IsNullOrWhiteSpace(input))
-            {
-                return true;
-            }
-            return false;
-        }
-        public bool VerificarAlteracaoValidaInt(string input, out int valor)
-        {
-            if (int.TryParse(input, out valor))
-            {
-                return true;
-            }
-            return false;
-        }
-
-        public bool VerificarAlteracaoDecimal(string input)
-        {
-            if (string.IsNullOrWhiteSpace(input))
-            {
-                return true;
-            }
-            return false;
-        }
-
-        public bool VerificarAlteracaoValidaDecimal(string input, out decimal valorAtualizado)
-        {
-            if (decimal.TryParse(input, out valorAtualizado))
-            {
-                return true;
-            }
-            return false;
-        }
-        public bool VerificarPreco(string input, out decimal valor)
-        {
-            valor = 0;
-            if (string.IsNullOrWhiteSpace(input))
-            {
-                return false;
-            }
-            if (!decimal.TryParse(input, out valor))
-            {
-                return false;
-            }
-            if (valor < 0)
-            {
-                return false;
-            }
-            return true;
-        }
 
         public bool VerificarAnoValido(Automovel automovel)
         {
@@ -159,35 +75,6 @@ namespace ConsoleApp2.Services
 
             return true;
         }
-        public bool VerificarValorInt(string input, out int valor)
-        {
-            valor = 0;
-            if (string.IsNullOrWhiteSpace(input))
-            {
-                return false;
-            }
-            if (!int.TryParse(input, out valor))
-            {
-                return false;
-            }
-            return true;
-        }
-        public bool VerificarBlindadoValido(string input, out bool blindado)
-        {
-            input.ToLower();
-            blindado = false;
-            if (input == "sim")
-            {
-                blindado = true;
-                return true;
-            }
-            else if (input == "nao" || input == "não")
-            {
-                blindado = false;
-                return true;
-            }
-
-            return false;
-        }
+        
     }
 }
